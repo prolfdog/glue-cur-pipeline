6680ad2b99f11bb29c69e7b13103a9ffa3135f29
prolfdog <36506731+prolfdog@users.noreply.github.com>
2025-10-27 09:35:40 +0000

    Reorganize project: separate customer-pipeline and finops-pipeline

--- customer-pipeline/terraform/s3.tf
+++ customer-pipeline/terraform/s3.tf
@@ -7,8 +7,8 @@ data "aws_s3_bucket" "glue_bucket" {
 resource "aws_s3_object" "customer_pipeline_script" {
   bucket = data.aws_s3_bucket.glue_bucket.id
   key    = "scripts/customer_pipeline.py"
-  source = "../scripts/customer_pipeline.py"
-  etag   = filemd5("../scripts/customer_pipeline.py")
+  source = "${path.module}/../scripts/customer_pipeline.py"
+  etag   = filemd5("${path.module}/../scripts/customer_pipeline.py")
 
   tags = {
     Name        = "customer-pipeline-script"
